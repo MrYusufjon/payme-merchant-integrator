7e8c4b04f758523863dd3564866a834928299f26
yusufjonnazarov <anorsystemmchj@list.ru>
2022-05-19 17:34:28 +0500

    check on create

--- src/controller/index.ts
+++ src/controller/index.ts
@@ -1,23 +1,23 @@
-import { Model } from "mongoose";
-import { ContructorDetails, PaymeIntegratorType } from "../app";
-import { ICounter } from "../db/models/counter";
-import { ITransaction } from "../db/models/transaction";
-import { PaymeErrors } from "../errors";
-import { PaymeBodyDto, paymeGroups, PaymeMethods, PaymeParamsDto } from "../validators/dtos/payme.dto";
-import { validateIt } from "../validators/validate";
+import {Model} from "mongoose";
+import {ContructorDetails,PaymeIntegratorType} from "../app";
+import {ICounter} from "../db/models/counter";
+import {ITransaction} from "../db/models/transaction";
+import {PaymeErrors} from "../errors";
+import {PaymeBodyDto,paymeGroups,PaymeMethods,PaymeParamsDto} from "../validators/dtos/payme.dto";
+import {validateIt} from "../validators/validate";
 
 export class MainController {
 
     static params: ContructorDetails;
     static TransactionModel: Model<ITransaction>;
     static CounterModel: Model<ICounter>;
-    public static async handler(request, TransactionModel: Model<ITransaction>, CounterModel: Model<ICounter>, params: ContructorDetails) {
+    public static async handler(request,TransactionModel: Model<ITransaction>,CounterModel: Model<ICounter>,params: ContructorDetails) {
         // initialize model
         this.params = params;
         this.TransactionModel = TransactionModel;
         this.CounterModel = CounterModel;
 
-        const body = await validateIt(request.body, PaymeBodyDto, paymeGroups);
+        const body = await validateIt(request.body,PaymeBodyDto,paymeGroups);
         return await this.switchMethod(body);
     }
 
@@ -53,7 +53,7 @@ export class MainController {
     }
 
     private static async CheckPerformTransaction(data) {
-        const params = await validateIt(data, PaymeParamsDto, [PaymeMethods.CheckPerformTransaction]);
+        const params = await validateIt(data,PaymeParamsDto,[PaymeMethods.CheckPerformTransaction]);
         params.amount = this.validatePaymeAmount(params.amount);
         const result = await this.params.isAccountExist(params.account);
         if (!result) {
@@ -72,8 +72,13 @@ export class MainController {
     }
 
     private static async CreateTransaction(data) {
-        const params = await validateIt(data, PaymeParamsDto, [PaymeMethods.CreateTransaction]);
+        const params = await validateIt(data,PaymeParamsDto,[PaymeMethods.CreateTransaction]);
         params.amount = this.validatePaymeAmount(params.amount);
+        const result = await this.params.isAccountExist(params.account);
+
+        if (!result) {
+            throw PaymeErrors.InvalidRequest('Account not found')
+        }
 
         if (this.params.type == PaymeIntegratorType.ONE_TIME) {
             const cost = await this.params.getPayingCost(params.account);
@@ -82,7 +87,7 @@ export class MainController {
             }
         }
 
-        let tra = await this.TransactionModel.findOne({ id: params.id });
+        let tra = await this.TransactionModel.findOne({id: params.id});
         if (tra) {
             return {
                 create_time: tra.create_time,
@@ -92,13 +97,13 @@ export class MainController {
         }
 
         if (this.params.type == PaymeIntegratorType.ONE_TIME) {
-            const is_exist = await this.TransactionModel.findOne({ account: params.account });
+            const is_exist = await this.TransactionModel.findOne({account: params.account});
             if (is_exist) {
                 throw PaymeErrors.InvalidRequest('Transaction not found')
             }
         }
         try {
-            const { value: transaction } = await this.CounterModel.findOneAndUpdate({ name: 'payme_transactions' }, { $inc: { value: 1 } }, { new: true, upsert: true });
+            const {value: transaction} = await this.CounterModel.findOneAndUpdate({name: 'payme_transactions'},{$inc: {value: 1}},{new: true,upsert: true});
 
             const tra_params = {
                 ...params,
@@ -106,11 +111,11 @@ export class MainController {
                 state: 1,
                 transaction
             }
-            const { _id: saved_id } = await new this.TransactionModel(tra_params).save();
+            const {_id: saved_id} = await new this.TransactionModel(tra_params).save();
             tra = await this.TransactionModel.findById(saved_id);
         } catch (error) {
             if (error.name == 'MongoError' && error.code == 11000) {
-                const exist = await this.TransactionModel.findOne({ account: params.account })
+                const exist = await this.TransactionModel.findOne({account: params.account})
                 if (!exist) {
                     throw PaymeErrors.InvalidRequest('Exists smth')
                 }
@@ -133,7 +138,7 @@ export class MainController {
     }
 
     private static async PerformTransaction(data) {
-        const params = await validateIt(data, PaymeParamsDto, [PaymeMethods.PerformTransaction])
+        const params = await validateIt(data,PaymeParamsDto,[PaymeMethods.PerformTransaction])
 
         const tra = await this.TransactionModel.findOne({
             id: params.id
@@ -154,12 +159,12 @@ export class MainController {
         }
         const time = new Date().getTime()
         // pay here first
-        await this.params.markAsPaid(tra.account, tra.amount);
+        await this.params.markAsPaid(tra.account,tra.amount);
         //
         tra.state = 2;
         tra.perform_time = time;
         await this.TransactionModel.updateOne(
-            { _id: tra._id },
+            {_id: tra._id},
             {
                 $set: {
                     perform_time: time,
@@ -176,8 +181,8 @@ export class MainController {
     }
 
     private static async CancelTransaction(data) {
-        const params = await validateIt(data, PaymeParamsDto, [PaymeMethods.CancelTransaction])
-        const tra = await this.TransactionModel.findOne({ id: params.id });
+        const params = await validateIt(data,PaymeParamsDto,[PaymeMethods.CancelTransaction])
+        const tra = await this.TransactionModel.findOne({id: params.id});
         if (!tra) {
             throw PaymeErrors.TransactionNotFound()
         }
@@ -199,7 +204,7 @@ export class MainController {
             cancel_params.state = tra.state * (-1);
             tra.state *= (-1);
         }
-        await this.TransactionModel.updateOne({ _id: tra._id }, { $set: cancel_params })
+        await this.TransactionModel.updateOne({_id: tra._id},{$set: cancel_params})
 
         await this.params.markAsCancel(tra.account);
 
@@ -211,8 +216,8 @@ export class MainController {
     }
 
     private static async CheckTransaction(data) {
-        const params = await validateIt(data, PaymeParamsDto, [PaymeMethods.CheckTransaction])
-        const tra = await this.TransactionModel.findOne({ id: params.id });
+        const params = await validateIt(data,PaymeParamsDto,[PaymeMethods.CheckTransaction])
+        const tra = await this.TransactionModel.findOne({id: params.id});
         if (!tra) {
             throw PaymeErrors.TransactionNotFound()
         }
